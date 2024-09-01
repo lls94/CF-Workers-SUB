22f2d1c42b812c2ccf7c4eedad803b460ee9ee95
李力士 <lilishi@chinasofti.com>
2024-09-01 20:09:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -32,7 +32,6 @@ MainData=[
 ].join('\n');
 
 let urls = [
-	
 ];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
