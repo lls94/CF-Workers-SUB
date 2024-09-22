cc32796e80a012fd169f53e84b83d69171c00e97
李力士 <lilishi@chinasofti.com>
2024-09-22 12:23:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,10 +16,10 @@ let cacheTTL = 24;//小时，缓存时长
 let MainData = `
 vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
 https://sub.xf.free.hr/auto
-tg://socks5?server=127.0.0.1&port=40000&remarks=WRAP+
 `
 
 MainData = [
+	"tg://socks5?server=127.0.0.1&port=40000&remarks=WRAP+",
 	"https://lls-edgetunnel.pages.dev/ac61f2ff-5755-4c82-8fdd-2563f0c81ce1", //lls-edgetunnel
 	"https://moistr.freenods.sbs/mianfeicf", //Moist_R 优选订阅
 	// "https://3k.fxxk.dedyn.io/auto",//CM 镜像订阅1
